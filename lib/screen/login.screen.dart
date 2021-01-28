039d9caa8c4f1118f684baef106e2cf706383e2f
unknown <n.donevski@yahoo.com>
2021-01-28 16:48:28 +0100

    added fingerprint on login screen

--- lib/screen/login.screen.dart
+++ lib/screen/login.screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:local_auth/auth_strings.dart';
+import 'package:local_auth/local_auth.dart';
 import './home.screen.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,6 +13,65 @@ class _LoginScreenState extends State<LoginScreen> {
   String _username;
   String _password;
 
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+
+  Future<bool> _isBiometricAvailable() async {
+    bool isAvailable = false;
+    try {
+      isAvailable = await _localAuthentication.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    if (!mounted) return isAvailable;
+
+    isAvailable
+        ? print('Biometric is available!')
+        : print('Biometric is unavailable.');
+
+    return isAvailable;
+  }
+
+  Future<void> _getListOfBiometricTypes() async {
+    List<BiometricType> availableBiometrics =
+        await _localAuthentication.getAvailableBiometrics();
+
+    print(availableBiometrics);
+
+    if (availableBiometrics.contains(BiometricType.face)) {
+      print('facee');
+    } else if (availableBiometrics.contains(BiometricType.fingerprint)) {
+      print('fingerprint');
+    }
+  }
+
+  Future<void> _authenticateUser() async {
+    bool isAuthenticated = false;
+    const ANDROID_AUTH_MESSAGES = AndroidAuthMessages(
+      signInTitle: 'Biometric Authentication',
+    );
+  
+    try {
+      isAuthenticated = await _localAuthentication.authenticateWithBiometrics(
+          localizedReason: "Please authenticate to view your images",
+          useErrorDialogs: true,
+          stickyAuth: true,
+          androidAuthStrings: ANDROID_AUTH_MESSAGES);
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    if (!mounted) return;
+
+    isAuthenticated
+        ? print('User is authenticated!')
+        : print('User is not authenticated.');
+
+    if (isAuthenticated) {
+      Navigator.pushNamed(context, HomeScreen.routeName);
+    }
+  }
+
   final _form = GlobalKey<FormState>();
 
   void _login() {
@@ -20,8 +81,6 @@ class _LoginScreenState extends State<LoginScreen> {
     }
     _form.currentState.save();
     Navigator.pushNamed(context, HomeScreen.routeName);
-    print(_username.length);
-    print(_password);
   }
 
   @override
@@ -54,30 +113,49 @@ class _LoginScreenState extends State<LoginScreen> {
                     onSaved: (val) => {_username = val},
                   ),
                   TextFormField(
-                    decoration: InputDecoration(labelText: 'Password'),
-                    textInputAction: TextInputAction.done,
-                    obscureText: true,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Enter Password';
-                      }
-                      if (value.length < 6) {
-                        return 'Password Should be at least 6 characters long.';
-                      }
-                      return null;
-                    },
-                  ),
+                      decoration: InputDecoration(labelText: 'Password'),
+                      textInputAction: TextInputAction.done,
+                      obscureText: true,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Enter Password';
+                        }
+                        if (value.length < 6) {
+                          return 'Password Should be at least 6 characters long.';
+                        }
+                        return null;
+                      },
+                      onSaved: (val) => {_password = val}),
                   Container(
                     margin: EdgeInsets.only(top: 50),
-                    child: RaisedButton(
-                      child: Text(
-                        'Submit',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      onPressed: _login,
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      splashColor: Colors.black,
+                    child: Container(
+                      child: Column(children: [
+                        RaisedButton(
+                          child: Text(
+                            'Login',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          onPressed: _login,
+                          color: Colors.blue,
+                          textColor: Colors.white,
+                          splashColor: Colors.black,
+                        ),
+                        RaisedButton(
+                          child: Text(
+                            'Login with biometics',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          onPressed: () async {
+                            if (await _isBiometricAvailable()) {
+                              await _getListOfBiometricTypes();
+                              await _authenticateUser();
+                            }
+                          },
+                          color: Colors.blue,
+                          textColor: Colors.white,
+                          splashColor: Colors.black,
+                        ),
+                      ]),
                     ),
                   )
                 ],
